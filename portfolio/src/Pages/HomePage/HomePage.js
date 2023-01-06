edb93e88374e3e14ceaffee340a304bd3e0baaf9
Stalius4 <deivydasrimgaila@gmail.com>
2023-01-06 14:36:44 +0000

    line added for navbar

--- portfolio/src/Pages/HomePage/HomePage.js
+++ portfolio/src/Pages/HomePage/HomePage.js
@@ -4,14 +4,15 @@ import "./mobile.css"
 const HomePage = () => {
   return (
  <div className='outer'>
+  <div className='horizontal-flex'>
+  <Link to="/"  className='home2 top-border-left'>HOME</Link>
+  <Link to="/about" className='about2'>ABOUT</Link>
+  <Link to="/contact" className='about2'>CONTACT</Link>
+  <Link to="/projects" className='about2 top-border-right'> PROJECTS</Link>
+  </div>
+
     <div className='main-container'>
-        <nav>
-      <Link to="/" className='nav-decoration home'>HOME</Link>
-      <Link to="/about" className='nav-decoration'>ABOUT</Link>
-      <Link to="/contact" className='nav-decoration'>CONTACT</Link>
-      <Link to="/projects" className='nav-decoration'>MY PROJECTS</Link>
-      </nav>
-    </div>
+   </div>
 </div>
   );
 };
