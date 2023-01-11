ab19b843ea4ec1e2021e09a488e9918150650235
Stalius4 <deivydasrimgaila@gmail.com>
2023-01-11 16:31:04 +0000

    text box added

--- portfolio/src/Pages/HomePage/HomePage.js
+++ portfolio/src/Pages/HomePage/HomePage.js
@@ -16,13 +16,23 @@ const HomePage = () => {
   </div>
 
     <div className='main-container'>
+<div className='home-text'>
+<div className='hi'> Hi,</div>
+<div> I'm Deivydas,</div>
 
+a front-end developer with a passion for creating great user experiences. <br/>I have experience with <br/> <spam className="white-bright"> HTML, CSS, JavaScript,</spam> and I am constantly learning new technologies. <br/>My portfolio showcases my projects, including responsive websites and web applications built with modern <br/>frameworks like React. 
+
+<div className='thanks'>Thank you for visiting!</div>
+
+</div>
 <button>MY CV</button>
-<footer>
-  <img src={gitpng} alt="git"  />
-  <img src={linkedinpng} alt="Linked in"  />
-  </footer>
+
    </div>
+   <footer>
+  <div className='left-footer'>  <img src={gitpng} alt="git"  /></div>
+<div className='right-footer'>  <img src={linkedinpng} alt="Linked in"/></div>
+
+  </footer>
 </div>
   );
 };
